800c65d804e7887a759bc3cafa4d805b6af9dd4b
charliemalro27 <carlos.maldonado@itgranada.com>
2022-01-19 13:09:56 +0100

    Hacer comentarios y cin(input)

--- dia2.cpp
+++ dia2.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main(){
   string saludo ="Hola,mundo";
 
-  cout<<saludo<<endl;
-  cout<<"Primera prueba de c++"
+  /*cout<<saludo<<endl;
+  cout<<"Primera prueba de c++";
+  */
+
+  cin >> repetir;
+  cout << repetir<<endl;
+
   return 0
-};
+}
