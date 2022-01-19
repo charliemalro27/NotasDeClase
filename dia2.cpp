e8105ec6c47d44fb3bb56282e7d90c696fdc73c9
charliemalro27 <carlos.maldonado@itgranada.com>
2022-01-19 13:30:30 +0100

    Bucle while mientras contraseña!=al bucle que le escribimos
    *nota= la exclamacion significa no en c++

--- dia2.cpp
+++ dia2.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main(){
   string saludo ="Hola,mundo";
-
+  string contrasena="AAA";
+  string repetir;
+  bool esCorrecto= false;
   /*cout<<saludo<<endl;
   cout<<"Primera prueba de c++";
   */
@@ -14,6 +16,14 @@ int main(){
   while (true){
   cin >> repetir >> endl;
   cout << repetir << endl;
+}
+  while (!esCorrecto){
+    cout<<"introduce contrasena";
+    cin >> repetir;
+    if (repetir==contrañesa){
+      esCorrecto=true;
+    }
+    cout << repetir << endl;
 }
   return 0
 }
